8e9c00e3fcd70d12a77ad4fa4adfa3f38035ff70
its-nedum <de.magnificent1@gmail.com>
2021-04-14 15:43:22 +0100

    feature(added api endpoints)

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Place;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Place::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'address' => 'required',
+            'coordinates' => 'required'
+        ]);
+
+        return Place::create($request->all());
+    }
+
+    /**
+     * Search the specified resource.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($address)
+    {
+        return Place::where('address', 'like', '%'.$address.'%')->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
